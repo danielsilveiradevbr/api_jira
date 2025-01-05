9f4b3444c34418f29cc5ab1cf632ceaabc0e81d5
danielzinhors <danielzinhors@gmail.com>
2025-01-05 11:46:55 -0300

    Adicionado avatars ao projeto

--- src/domain/model/dds/project.go
+++ src/domain/model/dds/project.go
@@ -12,6 +12,10 @@ type Project struct {
 	ID_JIRA      string
 	KEY_JIRA     string
 	DESCRICAO    string
+	Avatar_16x16 string
+	Avatar_24x24 string
+	Avatar_32x32 string
+	Avatar_48x48 string
 	DATA_CRIACAO time.Time
 	DATA_UPDATE  time.Time
 }
@@ -21,6 +25,10 @@ func NewProject(projetodto *project.Project) *Project {
 		ID_JIRA:      projetodto.ID,
 		KEY_JIRA:     projetodto.Key,
 		DESCRICAO:    projetodto.Name,
+		Avatar_16x16: projetodto.AvatarUrls.One6X16,
+		Avatar_24x24: projetodto.AvatarUrls.Two4X24,
+		Avatar_32x32: projetodto.AvatarUrls.Three2X32,
+		Avatar_48x48: projetodto.AvatarUrls.Four8X48,
 		DATA_CRIACAO: utils.GetADateTimeSaoPaulo(),
 	}
 }
